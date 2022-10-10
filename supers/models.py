8216bc6d17b43c87e073bf1f9b89025eea764be0
TSBrinker <tsbrinker@gmail.com>
2022-10-10 16:06:34 -0500

    made migrations in apps, connected mysql database, created superuser, and registered SuperType in admin.py

--- supers/models.py
+++ supers/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from super_types.models import SuperType
 
 # Create your models here.
+class Supers(models.Model):
+    name = models.CharField(max_length=40)
+    alter_ego = models.CharField(max_length=40)
+    primary_ability = models.CharField(max_length=100)
+    secondary_ability = models.CharField(max_length=100)
+    catchphrase = models.CharField(max_length=100)
+    super_type = models.ForeignKey(SuperType, on_delete=models.CASCADE)
