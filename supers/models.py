a21cc7a5b4c8bfd0a91bfb741fad63908310df15
TSBrinker <tsbrinker@gmail.com>
2022-10-11 11:12:49 -0500

    added admin registration for super, added serializer for superapp

--- supers/models.py
+++ supers/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from super_types.models import SuperType
 
 # Create your models here.
-class Supers(models.Model):
+class Super(models.Model):
     name = models.CharField(max_length=40)
     alter_ego = models.CharField(max_length=40)
     primary_ability = models.CharField(max_length=100)
